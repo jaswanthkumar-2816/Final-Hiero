b0c7de434fe51d337b9ce4769c573de9e57c98f2
Jaswanth Kumar <jaswanthkumar@192.168.1.9>
2025-11-28 01:35:16 +0530

    Ensure frontend-server serves public/index.html with PORT from env

--- jss/hiero/hiero last/frontend-server.js
+++ jss/hiero/hiero last/frontend-server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = 8082;
+const PORT = process.env.PORT || 8082;
 
 app.use(express.static(path.join(__dirname, "public")));
 
